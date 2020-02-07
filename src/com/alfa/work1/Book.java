ca5d3c27e162dd3ce227a847adaabc46509c72f4
Spajbl <sheekns@gmail.com>
2020-02-07 18:23:52 +0200

    Commit

--- src/com/alfa/work1/Book.java
+++ src/com/alfa/work1/Book.java
@@ -65,4 +65,16 @@ public class Book {
     public void setPrice(double price) {
         Price = price;
     }
+    public static void view(){
+        Book book = new Book();
+        book.setID(1);
+        book.setName("Name");
+        book.setAuthor("Author");
+        book.setPublishing("Publishing");
+        book.setYear(1999);
+        book.setPage(29);
+        book.setPrice(19.99);
+        System.out.print(book.getID() + " " + book.getName() + " " + book.getAuthor() + " " +
+                book.getPublishing() + " " + book.getYear() + " " + book.getPage() + " " + book.getPrice());
+    }
 }
--- src/com/alfa/work1/Runner.java
+++ src/com/alfa/work1/Runner.java
@@ -1,18 +1,9 @@
 package com.alfa.work1;
 
+import static com.alfa.work1.Book.view;
+
 public class Runner {
     public void run(){
-        Book book = new Book();
-        book.setID(1);
-        book.setName("Name");
-        book.setAuthor("Author");
-        book.setPublishing("Publishing");
-        book.setYear(1999);
-        book.setPage(29);
-        book.setPrice(19.99);
-
-        System.out.print(book.getID() + " " + book.getName() + " " + book.getAuthor() + " " +
-                book.getPublishing() + " " + book.getYear() + " " + book.getPage() + " " + book.getPrice() + " " );
-
+       view();
     }
 }
